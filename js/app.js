9f46d5cdc7e8928f4ef113c58c8f755c03220b9b
gusandrini <gustavosandrini92@gmail.com>
2024-06-28 17:18:33 -0300

    condicional para alterar status do botão

--- js/app.js
+++ js/app.js
@@ -6,9 +6,11 @@ function alterarStatus(id) {
     if (imagem.classList.contains('dashboard__item__img--rented')) {
         imagem.classList.remove('dashboard__item__img--rented');
         botao.textContent = 'Alugar';
+        botao.classList.remove('dashboard__item__button--return');
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
     }
 
     
