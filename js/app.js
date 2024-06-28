7058418e426a09ac77a91912cf0a909e33d02a66
gusandrini <gustavosandrini92@gmail.com>
2024-06-28 16:55:46 -0300

    criado função para identificar o jogo

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game${id}`);
+}
+
+
+
+
+
+
+// let = trocarStatus;
+
+// trocarStatus(){
+//     if () {
+//     inne= <a onclick="alterarStatus(1)" href="#" class="dashboard__item__button">Alugar</a>;
+//     } else {
+        
+//     }
+// }
