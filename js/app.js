635246952d265c843ee551c28041e5e5dbb7393e
gusandrini <gustavosandrini92@gmail.com>
2024-06-28 17:14:33 -0300

    adicionado condicionais aos botões

--- js/app.js
+++ js/app.js
@@ -5,8 +5,10 @@ function alterarStatus(id) {
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
         imagem.classList.remove('dashboard__item__img--rented');
+        botao.textContent = 'Alugar';
     } else {
         imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
     }
 
     
